465297a12af5581905fd85341ea1fa69f035e571
MananKakkar1 <manan.kakkar.2005@outlook.com>
2025-09-06 22:33:01 -0400

    Added another problem

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -3,27 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s1 = ""
-        curr = l1
-        while curr:
-            s1 += str(curr.val)
-            curr = curr.next
-        s2 = ""
-        curr = l2
-        while curr:
-            s2 += str(curr.val)
-            curr = curr.next
-        x = int(s1) + int(s2)
-        s3 = str(x)
-        s3 = s3[::-1]
-        newList = ListNode(int(s3[0]))
-        curr = newList
-        for char in s3[1:]:
-            curr.next = ListNode(int(char))
-            curr = curr.next
-        return newList
+def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    s1 = ""
+    curr = l1
+    while curr:
+        s1 += str(curr.val)
+        curr = curr.next
+    s2 = ""
+    curr = l2
+    while curr:
+        s2 += str(curr.val)
+        curr = curr.next
+    x = int(s1) + int(s2)
+    s3 = str(x)
+    s3 = s3[::-1]
+    newList = ListNode(int(s3[0]))
+    curr = newList
+    for char in s3[1:]:
+        curr.next = ListNode(int(char))
+        curr = curr.next
+    return newList
 
 # Time complexity: O(max(m, n)) where m and n are the lengths of the two linked lists.
 # Space complexity: O(max(m, n)) for the new linked list.
